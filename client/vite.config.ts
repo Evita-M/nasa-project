46293399376fe1e5cbca62df09caef1b38b367c6
Eva <eva@byallmeans.work>
2025-05-21 12:38:07 +0300

    refactor: update Vite configuration to set server port to 3000

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,17 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig, PluginOption } from "vite"
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig, PluginOption } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss() as PluginOption],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
